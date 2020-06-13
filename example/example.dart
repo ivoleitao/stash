df87a89d50ab8226fe8a9dced812855bcaa5ff5a
Ivo Leitão <ivo.leitao@gmail.com>
2020-06-13 16:14:28 +0100

    Fixed the example

--- example/example.dart
+++ example/example.dart
@@ -7,16 +7,6 @@ class Task {
 
   Task({this.id, this.title, this.completed = false});
 
-  /// Creates a [Task] from json map
-  factory Task.fromJson(Map<String, dynamic> json) => Task(
-      id: json['id'] as int,
-      title: json['title'] as String,
-      completed: json['completed'] as bool);
-
-  /// Creates a json map from a [Task]
-  Map<String, dynamic> toJson() =>
-      <String, dynamic>{'id': id, 'title': title, 'completed': completed};
-
   @override
   String toString() {
     return 'Task ${id}: "${title}" is ${completed ? "completed" : "not completed"}';
