4479b842d0f44468d1059600715ee3923af0fe9d
Ivo Leitão <ivo.leitao@sky.uk>
2021-04-18 18:08:37 +0100

    Restructured CI build

---
+++ tool/pana.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+MIN_SCORE=${1:-90} 
+PACKAGE=${PWD##*/} 
+
+SCORES=$(pana --no-warning 2>&1 | tail -1 | grep -o -E '[0-9]+\/[0-9]+')
+if [[ "$SCORES" =~ ([[:digit:]]*)\/([[:digit:]]*) ]]; then
+    GRANTED_POINTS=${BASH_REMATCH[1]}
+    MAX_POINTS=${BASH_REMATCH[2]} 
+
+    SCORE=$(( $GRANTED_POINTS * 100 / $MAX_POINTS ))
+    if (( $SCORE < $MIN_SCORE )); then
+        echo "score $SCORE% is bellow $MIN_SCORE%"
+        exit 1
+    else
+        echo "score: $SCORE%"
+    fi
+fi
+
+
