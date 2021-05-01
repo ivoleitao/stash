6f9b3f35e60544fd55d62a2e5e1585a528f08080
Ivo Leitão <ivo.leitao@sky.uk>
2021-05-01 10:04:30 +0100

    Added objectbox

---
+++ packages/stash_objectbox/lib/src/objectbox/objectbox_store.dart
@@ -0,0 +1,76 @@
+import 'package:stash/stash_api.dart';
+
+/// Objectbox based implemention of a [CacheStore]
+class ObjectboxStore extends CacheStore {
+  /// The function that converts between the Map representation to the
+  /// object stored in the cache
+  // final dynamic Function(Map<String, dynamic>)? _fromEncodable;
+
+  /// Builds a [ObjectboxStore].
+  ///
+  /// * [fromEncodable]: A custom function the converts to the object from a `Map<String, dynamic>` representation
+  ObjectboxStore({dynamic Function(Map<String, dynamic>)? fromEncodable})
+  /*: _fromEncodable = fromEncodable*/;
+
+  @override
+  Future<int> size(String name) => throw UnimplementedError();
+
+  @override
+  Future<Iterable<String>> keys(String name) => throw UnimplementedError();
+
+  @override
+  Future<Iterable<CacheStat>> stats(String name) => throw UnimplementedError();
+
+  @override
+  Future<Iterable<CacheEntry>> values(String name) =>
+      throw UnimplementedError();
+
+  @override
+  Future<bool> containsKey(String name, String key) =>
+      throw UnimplementedError();
+
+  @override
+  Future<CacheStat?> getStat(String name, String key) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Iterable<CacheStat?>> getStats(String name, Iterable<String> keys) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setStat(String name, String key, CacheStat stat) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CacheEntry?> getEntry(String name, String key) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> putEntry(String name, String key, CacheEntry entry) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> remove(String name, String key) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> clear(String name) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> delete(String name) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAll() {
+    throw UnimplementedError();
+  }
+}
---
+++ packages/stash_objectbox/lib/stash_objectbox.dart
@@ -0,0 +1,36 @@
+/// Provides a Objectbox implementation of the Stash caching API for Dart
+library stash_objectbox;
+
+import 'package:stash/stash_api.dart';
+import 'package:stash_objectbox/src/objectbox/objectbox_store.dart';
+
+export 'src/objectbox/objectbox_store.dart';
+
+/// Creates a new [Cache] backed by a [ObjectboxStore]
+///
+/// * [path]: The base storage location for this cache
+/// * [cacheName]: The name of the cache
+/// * [expiryPolicy]: The expiry policy to use, defaults to [EternalExpiryPolicy] if not provided
+/// * [sampler]: The sampler to use upon eviction of a cache element, defaults to [FullSampler] if not provided
+/// * [evictionPolicy]: The eviction policy to use, defaults to [LfuEvictionPolicy] if not provided
+/// * [maxEntries]: The max number of entries this cache can hold if provided. To trigger the eviction policy this value should be provided
+/// * [cacheLoader]: The [CacheLoader] that should be used to fetch a new value upon expiration
+/// * [fromEncodable]: A custom function the converts to the object from a `Map<String, dynamic>` representation
+///
+/// Returns a new [Cache] backed by a [ObjectboxStore]
+Cache newObjectboxCache(String path,
+    {String? cacheName,
+    ExpiryPolicy? expiryPolicy,
+    KeySampler? sampler,
+    EvictionPolicy? evictionPolicy,
+    int? maxEntries,
+    CacheLoader? cacheLoader,
+    dynamic Function(dynamic)? fromEncodable}) {
+  return Cache.newCache(ObjectboxStore(fromEncodable: fromEncodable),
+      name: cacheName,
+      expiryPolicy: expiryPolicy,
+      sampler: sampler,
+      evictionPolicy: evictionPolicy,
+      maxEntries: maxEntries,
+      cacheLoader: cacheLoader);
+}
