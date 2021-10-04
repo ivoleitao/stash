050d367a9eff030d21d647d7ca9712aaee9ea131
Ivo Leitão <ivo.leitao@gmail.com>
2021-10-05 00:06:46 +0100

    Added Vault support to objectbox

--- packages/stash_objectbox/lib/stash_objectbox.dart
+++ packages/stash_objectbox/lib/stash_objectbox.dart
@@ -10,6 +10,68 @@ import 'package:stash_objectbox/src/objectbox/objectbox_store.dart';
 export 'src/objectbox/objectbox_adapter.dart';
 export 'src/objectbox/objectbox_store.dart';
 
+/// Creates a new [ObjectboxVaultStore]
+///
+/// * [path]: The base storage location for this store
+/// * [codec]: The [StoreCodec] used to convert to/from a Map<String, dynamic>` representation to a binary representation
+/// * [fromEncodable]: A custom function the converts to the object from a `Map<String, dynamic>` representation
+/// * [maxDBSizeInKB]: The max DB size
+/// * [fileMode]: The file mode
+/// * [maxReaders]: The number of maximum readers
+/// * [queriesCaseSensitiveDefault]: If the queries are case sensitive, the default is true
+ObjectboxVaultStore newObjectboxLocalVaultStore(
+    {String? path,
+    StoreCodec? codec,
+    dynamic Function(Map<String, dynamic>)? fromEncodable,
+    int? maxDBSizeInKB,
+    int? fileMode,
+    int? maxReaders,
+    bool? queriesCaseSensitiveDefault}) {
+  return ObjectboxVaultStore(
+      ObjectboxAdapter(path ?? Directory.systemTemp.path,
+          maxDBSizeInKB: maxDBSizeInKB,
+          fileMode: fileMode,
+          maxReaders: maxReaders,
+          queriesCaseSensitiveDefault: queriesCaseSensitiveDefault),
+      codec: codec,
+      fromEncodable: fromEncodable);
+}
+
+/// Creates a new [ObjectboxCacheStore]
+///
+/// * [path]: The base storage location for this store
+/// * [codec]: The [StoreCodec] used to convert to/from a Map<String, dynamic>` representation to a binary representation
+/// * [fromEncodable]: A custom function the converts to the object from a `Map<String, dynamic>` representation
+/// * [maxDBSizeInKB]: The max DB size
+/// * [fileMode]: The file mode
+/// * [maxReaders]: The number of maximum readers
+/// * [queriesCaseSensitiveDefault]: If the queries are case sensitive, the default is true
+ObjectboxCacheStore newObjectboxLocalCacheStore(
+    {String? path,
+    StoreCodec? codec,
+    dynamic Function(Map<String, dynamic>)? fromEncodable,
+    int? maxDBSizeInKB,
+    int? fileMode,
+    int? maxReaders,
+    bool? queriesCaseSensitiveDefault}) {
+  return ObjectboxCacheStore(
+      ObjectboxAdapter(path ?? Directory.systemTemp.path,
+          maxDBSizeInKB: maxDBSizeInKB,
+          fileMode: fileMode,
+          maxReaders: maxReaders,
+          queriesCaseSensitiveDefault: queriesCaseSensitiveDefault),
+      codec: codec,
+      fromEncodable: fromEncodable);
+}
+
+/// Creates a new [Vault] backed by a [ObjectboxVaultStore]
+///
+/// * [store]: An existing objectbox store
+/// * [vaultName]: The name of the vault
+Vault<T> _newObjectboxVault<T>(ObjectboxVaultStore store, {String? vaultName}) {
+  return Vault<T>.newVault(store, name: vaultName);
+}
+
 /// Creates a new [Cache] backed by a [ObjectboxCacheStore]
 ///
 /// * [store]: An existing objectbox store
@@ -38,31 +100,39 @@ Cache<T> _newObjectboxCache<T>(ObjectboxCacheStore store,
       eventListenerMode: eventListenerMode);
 }
 
-/// Creates a new [ObjectboxCacheStore]
+/// Creates a new [Vault] backed by a [ObjectboxVaultStore]
 ///
-/// * [path]: The base storage location for this store
+/// * [vaultName]: The name of the vault
+/// * [store]: An existing store, note that [codec], [fromEncodable], [path], [maxDBSizeInKB], [fileMode], [maxReaders] and [queriesCaseSensitiveDefault] will be all ignored is this parameter is provided
+/// * [path]: The base storage location for this cache
 /// * [codec]: The [StoreCodec] used to convert to/from a Map<String, dynamic>` representation to a binary representation
 /// * [fromEncodable]: A custom function the converts to the object from a `Map<String, dynamic>` representation
 /// * [maxDBSizeInKB]: The max DB size
 /// * [fileMode]: The file mode
 /// * [maxReaders]: The number of maximum readers
 /// * [queriesCaseSensitiveDefault]: If the queries are case sensitive, the default is true
-ObjectboxCacheStore newObjectboxCacheStore(
+///
+/// Returns a new [Vault] backed by a [ObjectboxVaultStore]
+Vault<T> newObjectBoxVault<T>(
     {String? path,
+    String? vaultName,
+    ObjectboxVaultStore? store,
     StoreCodec? codec,
     dynamic Function(Map<String, dynamic>)? fromEncodable,
     int? maxDBSizeInKB,
     int? fileMode,
     int? maxReaders,
     bool? queriesCaseSensitiveDefault}) {
-  return ObjectboxCacheStore(
-      ObjectboxAdapter(path ?? Directory.systemTemp.path,
+  return _newObjectboxVault<T>(
+      newObjectboxLocalVaultStore(
+          path: path,
+          codec: codec,
+          fromEncodable: fromEncodable,
           maxDBSizeInKB: maxDBSizeInKB,
           fileMode: fileMode,
           maxReaders: maxReaders,
           queriesCaseSensitiveDefault: queriesCaseSensitiveDefault),
-      codec: codec,
-      fromEncodable: fromEncodable);
+      vaultName: vaultName);
 }
 
 /// Creates a new [Cache] backed by a [ObjectboxCacheStore]
@@ -83,7 +153,7 @@ ObjectboxCacheStore newObjectboxCacheStore(
 /// * [maxReaders]: The number of maximum readers
 /// * [queriesCaseSensitiveDefault]: If the queries are case sensitive, the default is true
 ///
-/// Returns a new [Cache] backed by a [ObjectboxStore]
+/// Returns a new [Cache] backed by a [ObjectboxCacheStore]
 Cache<T> newObjectBoxCache<T>(
     {String? path,
     String? cacheName,
@@ -101,7 +171,7 @@ Cache<T> newObjectBoxCache<T>(
     int? maxReaders,
     bool? queriesCaseSensitiveDefault}) {
   return _newObjectboxCache<T>(
-      newObjectboxCacheStore(
+      newObjectboxLocalCacheStore(
           path: path,
           codec: codec,
           fromEncodable: fromEncodable,
@@ -118,9 +188,17 @@ Cache<T> newObjectBoxCache<T>(
       eventListenerMode: eventListenerMode);
 }
 
-/// Extension over [ObjectboxStore] allowing the creation of multiple caches from
+/// Extension over [ObjectboxVaultStore] allowing the creation of multiple vaults from
 /// the same store
-extension ObjectboxStoreExtension on ObjectboxCacheStore {
+extension ObjectboxVaultStoreExtension on ObjectboxVaultStore {
+  Vault<T> vault<T>({String? vaultName}) {
+    return _newObjectboxVault<T>(this, vaultName: vaultName);
+  }
+}
+
+/// Extension over [ObjectboxCacheStore] allowing the creation of multiple caches from
+/// the same store
+extension ObjectboxCacheStoreExtension on ObjectboxCacheStore {
   Cache<T> cache<T>(
       {String? cacheName,
       KeySampler? sampler,
