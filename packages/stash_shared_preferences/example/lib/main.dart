a1d5921d61fe3d585b27e0e07681299bf2155a9f
Ivo Leitão <ivo.leitao@gmail.com>
2023-01-29 14:27:41 +0000

    fix: better example

--- packages/stash_shared_preferences/example/lib/main.dart
+++ packages/stash_shared_preferences/example/lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:stash/stash_api.dart';
 import 'package:stash_shared_preferences/stash_shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class Counter {
+  final int value;
+  final DateTime updateTime;
+
+  Counter({required this.value, required this.updateTime});
+
+  /// Creates a [Counter] from json map
+  factory Counter.fromJson(Map<String, dynamic> json) => Counter(
+      value: json['value'] as int,
+      updateTime: DateTime.parse(json['timestamp'] as String));
+
+  /// Creates a json map from a [Task]
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'value': value,
+        'timestamp': updateTime.toIso8601String()
+      };
 }
 
 class MyApp extends StatelessWidget {
@@ -30,25 +49,33 @@ class SharedPreferencesDemo extends StatefulWidget {
 }
 
 class SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
-  final Future<Vault<int>> _vault = newSharedPreferencesVaultStore()
-      .then((store) => store.vault<int>(name: 'vault'));
-  late Future<int> _counter;
+  final Future<Vault<Counter>> _vault = newSharedPreferencesVaultStore().then(
+      (store) => store.vault<Counter>(
+          name: 'vault', fromEncodable: (json) => Counter.fromJson(json)));
+  late Future<Counter> _counter;
+
+  Future<Counter> _getCounter([Vault<Counter>? vault]) {
+    final v = vault == null ? _vault : Future.value(vault);
+
+    return v.then((vault) => vault.get('counter')).then(
+        (counter) => counter ?? Counter(value: 0, updateTime: DateTime.now()));
+  }
 
   Future<void> _incrementCounter() async {
-    final Vault<int> vault = await _vault;
-    final int counter = (await vault.get('counter') ?? 0) + 1;
+    final Vault<Counter> vault = await _vault;
+    final Counter currentCounter = await _getCounter(vault);
 
     setState(() {
-      _counter = vault.put('counter', counter).then((value) => counter);
+      final newCounter =
+          Counter(value: currentCounter.value + 1, updateTime: DateTime.now());
+      _counter = vault.put('counter', newCounter).then((value) => newCounter);
     });
   }
 
   @override
   void initState() {
     super.initState();
-    _counter = _vault.then((Vault<int> vault) {
-      return vault.get('counter').then((value) => value ?? 0);
-    });
+    _counter = _getCounter();
   }
 
   @override
@@ -58,9 +85,9 @@ class SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
         title: const Text('SharedPreferences Demo'),
       ),
       body: Center(
-          child: FutureBuilder<int>(
+          child: FutureBuilder<Counter>(
               future: _counter,
-              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              builder: (BuildContext context, AsyncSnapshot<Counter> snapshot) {
                 switch (snapshot.connectionState) {
                   case ConnectionState.none:
                   case ConnectionState.waiting:
@@ -70,9 +97,14 @@ class SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
                     if (snapshot.hasError) {
                       return Text('Error: ${snapshot.error}');
                     } else {
+                      final count = snapshot.data?.value ?? 0;
+                      final updateTime = snapshot.data?.updateTime;
+                      final lastPressed = updateTime != null
+                          ? ' (last pressed on ${DateFormat('dd-MM-yyyy HH:mm:ss').format(updateTime)})'
+                          : '';
                       return Text(
-                        'Button tapped ${snapshot.data} time${snapshot.data == 1 ? '' : 's'}.\n\n'
-                        'This should persist across restarts.',
+                        'Button tapped $count time${count == 1 ? '' : 's'}.\n\n'
+                        'This should persist across restarts$lastPressed.',
                       );
                     }
                 }
